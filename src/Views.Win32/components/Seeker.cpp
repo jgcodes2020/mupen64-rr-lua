5bd9ba1a1acdffe592f31f43dcdcb077f51a882d
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-06-14 15:41:39 +0200

    refactor(Seeker): clean up Seeker.cpp

--- src/Views.Win32/components/Seeker.cpp
+++ src/Views.Win32/components/Seeker.cpp
@@ -11,122 +11,120 @@
 
 #define WM_SEEK_COMPLETED (WM_USER + 11)
 
-namespace Seeker
+struct seeker_state {
+    HWND hwnd{};
+    UINT_PTR refresh_timer{};
+};
+
+static seeker_state seeker{};
+
+static LRESULT CALLBACK dlgproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
-    HWND current_hwnd;
-    UINT_PTR refresh_timer;
-
-    LRESULT CALLBACK SeekerProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+    switch (msg)
     {
-        switch (Message)
+    case WM_INITDIALOG:
+        seeker.hwnd = hwnd;
+
+        SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Idle");
+        SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
+        SetDlgItemText(hwnd, IDC_SEEKER_FRAME, g_config.seeker_value.c_str());
+
+        seeker.refresh_timer = SetTimer(hwnd, NULL, 1000 / 10, nullptr);
+        SetFocus(GetDlgItem(hwnd, IDC_SEEKER_FRAME));
+        break;
+    case WM_DESTROY:
+        KillTimer(hwnd, seeker.refresh_timer);
+        g_core_ctx->vcr_stop_seek();
+        break;
+    case WM_CLOSE:
+        EndDialog(hwnd, IDCANCEL);
+        break;
+    case WM_SEEK_COMPLETED:
+        SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Seek completed");
+        SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
+        SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"");
+        break;
+    case WM_TIMER:
         {
-        case WM_INITDIALOG:
-            current_hwnd = hwnd;
-
-            SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Idle");
-            SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
-            SetDlgItemText(hwnd, IDC_SEEKER_FRAME, g_config.seeker_value.c_str());
-
-            refresh_timer = SetTimer(hwnd, NULL, 1000 / 10, nullptr);
-            SetFocus(GetDlgItem(hwnd, IDC_SEEKER_FRAME));
+            if (!g_core_ctx->vcr_is_seeking())
+            {
+                break;
+            }
+            std::pair<size_t, size_t> pair;
+            g_core_ctx->vcr_get_seek_completion(pair);
+            auto [current, total] = pair;
+            const auto str = std::format(L"Seeked {:.2f}%", static_cast<float>(current) / static_cast<float>(total) * 100.0);
+            SetDlgItemText(hwnd, IDC_SEEKER_STATUS, str.c_str());
             break;
-        case WM_DESTROY:
-            KillTimer(hwnd, refresh_timer);
-            g_core_ctx->vcr_stop_seek();
+        }
+    case WM_COMMAND:
+        switch (LOWORD(wparam))
+        {
+        case IDC_SEEKER_FRAME:
+            {
+                wchar_t str[260] = {0};
+                GetDlgItemText(hwnd, IDC_SEEKER_FRAME, str, std::size(str));
+                g_config.seeker_value = str;
+            }
             break;
-        case WM_CLOSE:
+        case IDC_SEEKER_START:
+            {
+                if (g_core_ctx->vcr_is_seeking())
+                {
+                    g_core_ctx->vcr_stop_seek();
+                    break;
+                }
+
+                SetDlgItemText(hwnd, IDC_SEEKER_START, L"Stop");
+                if (g_config.core.seek_savestate_interval == 0)
+                {
+                    SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"Seek savestates disabled. Seeking backwards will be slower.");
+                }
+
+                if (g_core_ctx->vcr_begin_seek(g_config.seeker_value, true) != Res_Ok)
+                {
+                    SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
+                    SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Couldn't seek");
+                    SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"");
+                    break;
+                }
+
+                break;
+            }
+        case IDCANCEL:
             EndDialog(hwnd, IDCANCEL);
-            break;
-        case WM_SEEK_COMPLETED:
-            SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Seek completed");
-            SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
-            SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"");
-            break;
-        case WM_TIMER:
-            {
-                if (!g_core_ctx->vcr_is_seeking())
-                {
-                    break;
-                }
-                std::pair<size_t, size_t> pair;
-                g_core_ctx->vcr_get_seek_completion(pair);
-                auto [current, total] = pair;
-                const auto str = std::format(L"Seeked {:.2f}%", static_cast<float>(current) / static_cast<float>(total) * 100.0);
-                SetDlgItemText(hwnd, IDC_SEEKER_STATUS, str.c_str());
-                break;
-            }
-        case WM_COMMAND:
-            switch (LOWORD(wParam))
-            {
-            case IDC_SEEKER_FRAME:
-                {
-                    wchar_t str[260] = {0};
-                    GetDlgItemText(hwnd, IDC_SEEKER_FRAME, str, std::size(str));
-                    g_config.seeker_value = str;
-                }
-                break;
-            case IDC_SEEKER_START:
-                {
-                    if (g_core_ctx->vcr_is_seeking())
-                    {
-                        g_core_ctx->vcr_stop_seek();
-                        break;
-                    }
-
-                    SetDlgItemText(hwnd, IDC_SEEKER_START, L"Stop");
-                    if (g_config.core.seek_savestate_interval == 0)
-                    {
-                        SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"Seek savestates disabled. Seeking backwards will be slower.");
-                    }
-
-                    if (g_core_ctx->vcr_begin_seek(g_config.seeker_value, true) != Res_Ok)
-                    {
-                        SetDlgItemText(hwnd, IDC_SEEKER_START, L"Start");
-                        SetDlgItemText(hwnd, IDC_SEEKER_STATUS, L"Couldn't seek");
-                        SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, L"");
-                        break;
-                    }
-
-                    break;
-                }
-            case IDCANCEL:
-                EndDialog(hwnd, IDCANCEL);
-                break;
-            default:
-                break;
-            }
             break;
         default:
             break;
         }
-        return FALSE;
+        break;
+    default:
+        break;
+    }
+    return FALSE;
+}
+
+void Seeker::show()
+{
+    // Let's not have 2 instances of a singleton...
+    if (seeker.hwnd)
+    {
+        return;
     }
 
-    void show()
-    {
-        // Let's not have 2 instances of a singleton...
-        if (current_hwnd)
-        {
+    // We need to run the dialog on another thread, since we don't want to block the main one
+    std::thread([] {
+        DialogBox(g_app_instance, MAKEINTRESOURCE(IDD_SEEKER), g_main_hwnd, (DLGPROC)dlgproc);
+        seeker.hwnd = nullptr;
+    })
+    .detach();
+}
+
+void Seeker::init()
+{
+    Messenger::subscribe(Messenger::Message::SeekCompleted, [](std::any) {
+        if (!seeker.hwnd)
             return;
-        }
-
-        // We need to run the dialog on another thread, since we don't want to block the main one
-        std::thread([] {
-            DialogBox(g_app_instance,
-                      MAKEINTRESOURCE(IDD_SEEKER),
-                      g_main_hwnd,
-                      (DLGPROC)SeekerProc);
-            current_hwnd = nullptr;
-        })
-        .detach();
-    }
-
-    void init()
-    {
-        Messenger::subscribe(Messenger::Message::SeekCompleted, [](std::any) {
-            if (!current_hwnd)
-                return;
-            SendMessage(current_hwnd, WM_SEEK_COMPLETED, 0, 0);
-        });
-    }
-} // namespace Seeker
+        SendMessage(seeker.hwnd, WM_SEEK_COMPLETED, 0, 0);
+    });
+}
