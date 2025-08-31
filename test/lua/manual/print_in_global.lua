f7cebe3061a796b72b54fdacc3829d4f28c60ff4
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-08-31 17:49:54 +0200

    test(Lua): manual test for printing in global execution context

---
+++ test/lua/manual/print_in_global.lua
@@ -0,0 +1,12 @@
+--
+-- Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+--
+-- SPDX-License-Identifier: GPL-2.0-or-later
+--
+
+-- Start the script, and ensure that:
+-- 1. The message "works" is printed to the console
+
+dofile(debug.getinfo(1).source:sub(2):gsub("\\[^\\]+\\[^\\]+$", "") .. '\\test_prelude.lua')
+
+print('works')
